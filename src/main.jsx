a4a19cb0fb052c24b3ea5bb99a4acb02e6f2ce50
Atokolo John <atokoloj@gmail.com>
2025-05-16 10:46:41 +0100

    image display

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const madara = <img src="/src/assets/-Madara-Uchiha.jpg" alt="" srcset="">
+
+function Displayimage (){
+  return(
+    madara
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Displayimage />
   </StrictMode>,
 )
